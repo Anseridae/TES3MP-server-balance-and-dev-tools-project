5e990817734d3f7c75f5100ccf7862a793901536
Anseri1 <99191975+Anseri1@users.noreply.github.com>
2022-02-10 05:29:33 -0600

    Add files via upload
    made the code a lot better

--- statsTools/main.py
+++ statsTools/main.py
@@ -8,8 +8,9 @@ def main():
           "E.g. instead of doing 70% you do 0.7 or just .70 if that's how you prefer to write it\n"
           "then enter in the number of attempts you want to simulate\n")
 
-    stats_tools.exactDistributionCalc(float(input("plz enter the chance for one success: ")),
-                                      int(input("plz enter the number of attempts you wanna simulate: ")))
+    stats_tools.distribution = \
+        stats_tools.exactDistributionCalc(float(input("plz enter the chance for one success: ")),
+                                          int(input("plz enter the number of attempts you wanna simulate: ")))
     print("\nhere is the binomial distribution")
     stats_tools.printExact()
     print("\n"
